9f22883d239ec2a6c89dfd4cc098eebe8956cdfb
Kelsey Huse <kelseyhuse30@gmail.com>
2017-04-14 12:24:16 -0400

    deleted files/folders not needed and set up config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+class ApplicationController < Sinatra::Base
+
+	configure do
+		set :views, 'app/views'
+		enable :sessions
+		set :session_secret, "password_security"
+	end
+
+	get '/' do
+		erb :index
+	end
+  
 end
