9c8f2bdc1fb2f74364057f5343b845e8e4903f75
Kelsey Huse <kelseyhuse30@gmail.com>
2017-06-26 20:19:11 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,103 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users =	User.create([
+		{ 
+		username: 'kel-c',
+		email: 'kelc@email.com'
+		password_digest: BCrypt::Password.create('passw0rd')},
+		{ 
+		username: 'dad',
+		email: 'dadiscool@email.com'
+		password_digest: BCrypt::Password.create('passw0rd')},
+		{
+		username: 'wonderwoman',
+		email: 'wonderwoman@email.com'
+		password_digest: BCrypt::Password.create('passw0rd')}
+	])
+
+categories = Category.create([
+	{
+		name: 'Wonders of the World' },
+	{
+		name: "Kelsey's Philly Bucketlist" },
+	{
+		name: 'Best of US' }
+	])
+
+items = Item.create([
+	{
+		name: 'Christ the Redeemer',
+		description: 'Statue of Jesus',
+		location: 'Brazil',
+		user_id: 3,
+		category_id: 1
+		},
+	{
+		name: 'Colosseum',
+		description: 'Amphitheatre in Rome',
+		location: 'Italy',
+		user_id: 3,
+		category_id: 1
+		},
+	{
+		name: 'Taj Mahal',
+		description: 'Mausoleum in Agra',
+		location: 'India',
+		user_id: 3,
+		category_id: 1
+		},
+	{
+		name: 'Great Wall of China',
+		description: '4,500mi wall',
+		location: 'China',
+		user_id: 3,
+		category_id: 1
+		},
+	{
+		name: 'Petra',
+		description: 'Ancient Temple',
+		location: 'Jordan',
+		user_id: 3,
+		category_id: 1
+		},
+	{
+		name: 'Machu Picchu',
+		description: 'Ancient Incan City',
+		location: 'Wonders',
+		user_id: 3,
+		category_id: 1
+		},
+	{
+		name: 'Zahav',
+		description: 'Mediterranean food',
+		location: 'Philadelphia',
+		user_id: 1,
+		category_id: 2
+		},
+	{
+		name: 'Vernick Food and Drink',
+		description: 'American Fine Dining',
+		location: 'Philadelphia',
+		user_id: 1,
+		category_id: 2
+		},
+	{
+		name: "Talula's Garden",
+		description: 'Farm Fresh Restaurant',
+		location: 'Philadelphia',
+		user_id: 1,
+		category_id: 2
+		},
+	{
+		name: "Walk Across the Panhandle",
+		description: 'Walk from North to South',
+		location: 'Oklahoma',
+		user_id: 2,
+		category_id: 3
+		},
+	{
+		name: "Route 66",
+		description: 'Famous highway',
+		location: 'Western US',
+		user_id: 2,
+		category_id: 3
+		}
+])
